90a0c21facf79a2a959aaede45eef097079ea918
pikson <goverperson@gmail.com>
2020-12-21 22:29:42 +0800

    create go.mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/piksonGit/pmongo/v1
+
+go 1.14
+
+require go.mongodb.org/mongo-driver v1.4.4 // indirect
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+import(
+	"go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+    "go.mongodb.org/mongo-driver/mongo/readpref"
+)
+func main(){
+
+}
