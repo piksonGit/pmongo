cd7ea9008b6a03e9776152464ef5527c05e9734c
peter <goverperson@gmail.com>
2020-12-25 15:34:52 +0800

    完成

---
+++ db/db_test.go
@@ -0,0 +1,9 @@
+package db
+
+import (
+	"testing"
+)
+
+func BenchmarkFind(b *testing.B) {
+
+}
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+type Ranran struct {
+	Mouse string
+	Brain string
+}
+type Qijing struct {
+	*Ranran
+	Name    string
+	Age     int
+	Address string
+}
+
+func main() {
+
+}
